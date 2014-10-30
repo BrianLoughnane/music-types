8c73d60aa47cfdfd52e685c7366e0a4dbd8d05cc
BrianLoughnane <BrianJLoughnane@gmail.com>
2014-10-30 11:33:50 -0400

    hands refactored, styling added

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
 var myApp = angular.module('myApp', []);
 myApp.controller('myController', function($scope) {
 	$scope.progress = 0;
+	$scope.score = 0;
+	$scope.word = 'ready';
+	$scope.song = 'song';
+	$scope.artist = 'artist';
 });
