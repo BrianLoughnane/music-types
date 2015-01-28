72a9aa2ab1939324ad01105165eb52b45ad5b603
BrianLoughnane <BrianJLoughnane@gmail.com>
2015-01-28 16:41:58 -0500

    refactored fingers into directives

--- public/mainAndKeyboard/mainAndKeyboard.js
+++ public/mainAndKeyboard/mainAndKeyboard.js
@@ -201,3 +201,113 @@ myApp.controller('MainCtrl', function($scope, $http, $sce, $location, $timeout, 
 
 
 });  //end MainCtrl
+
+myApp
+	// .directive('LP', function() {
+	// 	var letters = 
+	// 	return {
+	// 		restrict: 'A',
+	// 		link: function(s,e,a) {
+
+	// 		}	
+	// 	}
+	// })
+
+
+
+// fingerDirectives.js
+
+// set data on the element
+// this directive watches for letters,
+// generates a listener
+
+
+	.directive('finger', function() {
+		var dictionary = {
+			leftPinky : 'qazQAZ`~1!^&*()_+{}:?HJKLNM<>YUIOP',
+			leftRing : 'wsxWSX2@',
+			leftMiddle : 'edcEDC3#',
+			leftIndex : 'rtfgvbRTFGVB45$%',
+			thumb : ' ',
+			rightIndex : 'yuhjnmYUHJNM67^&',
+			rightMiddle : 'ikIK8,<*',
+			rightRing : 'olOL9.>(',
+			rightPinky : '\'pP0)-_;:/=]+[{}|?!@#$%QWERTASDFGZXCVB~'
+		}
+		// var letters = 'wsxWSX2@'.split('');
+		return {
+			restrict: 'A',
+			link: function(s,e,a) {
+				function generateLetterListener (letters, e) {
+					return function (newGroupArray) {
+						var currentLetter = newGroupArray[0];
+						var mistype = newGroupArray[1];
+						
+						if(letters.indexOf(currentLetter) != -1 && !mistype) {
+							e.addClass('current');
+						} else {
+							e.removeClass('current');
+						}
+					}
+				}
+				var letters = dictionary[a.finger].split('');
+				s.$watchGroup(
+					['currentLetter', 'mistype'], 
+					generateLetterListener(letters, e)
+				);
+			}
+		}
+	})
+
+
+
+
+
+// HelperFunctions.js
+
+// var HelperFunction = {
+// 	generateLetterListener: function (letters, e) {
+// 		return function (newGroupArray) {
+// 			var currentLetter = newGroupArray[0];
+// 			var mistype = newGroupArray[1];
+			
+// 			if(letters.indexOf(currentLetter) != -1 && !mistype) {
+// 				e.addClass('current');
+// 			} else {
+// 				e.removeClass('current');
+// 			}
+// 		}
+// 	},
+
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
