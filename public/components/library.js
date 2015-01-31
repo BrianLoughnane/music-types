9a17946b08f24ca6602cebb4ca0757f6ed70104c
BrianLoughnane <BrianJLoughnane@gmail.com>
2015-01-31 16:36:43 -0500

    refactored search controller

--- public/components/library.js
+++ public/components/library.js
@@ -11,8 +11,8 @@ myApp.factory('nowPlaying', function() {
 	}
 });
 
-myApp.factory('searchSong', function($http, $q) {
-	return function(val) {
+myApp.factory('searchSong', function ($http, $q) {
+	return function (val) {
 		var defer = $q.defer();
 		$http.get('musicmatch/' + val)
 			.success(function(response) {
@@ -21,6 +21,69 @@ myApp.factory('searchSong', function($http, $q) {
 		return defer.promise;
 	}
 });
+
+myApp.factory('querySong', function (searchSong) {
+	return function (ctrl) {
+		return function (q) {
+			var value = q.trim();
+			if(value.length) {
+				searchSong(value)
+				.then(function (response) {
+					var tracks = response.message.body.track_list;
+					var goodTracks = [];
+
+					for (var i = 0; i < tracks.length; i++) {
+						if(tracks[i].track.track_spotify_id && tracks[i].track.has_lyrics) {
+							goodTracks.push(tracks[i]);
+						}
+					}
+					ctrl.searchResults = goodTracks;
+				});
+			}	
+		};
+	};
+});
+
+myApp.factory('getSong', function (nowPlaying, getLyrics, $location) {
+	return function (track) {
+		nowPlaying.artist = track.artist_name;
+		nowPlaying.song = track.track_name;
+		nowPlaying.album = track.album_name;
+		nowPlaying.art = track.album_coverart_500x500;
+		nowPlaying.spotifyId = track.track_spotify_id;
+
+		getLyrics(track.track_id)
+			.then(function(response) {
+
+				// oddly, this had to change from response.data.message..... to response.message....
+				// var tracking = response.data.message.body.lyrics.pixel_tracking_url;
+				// var lyrics = response.data.message.body.lyrics.lyrics_body;
+				var tracking = response.message.body.lyrics.pixel_tracking_url;
+				var lyrics = response.message.body.lyrics.lyrics_body;
+
+				console.log('lyrics_body', lyrics);
+
+				lyrics = lyrics.replace(/’/g, "'");
+				lyrics = lyrics.replace(/‘/g, "'");
+				lyrics = lyrics.replace(/'/g, "\'");
+				lyrics = lyrics.replace(/ \n\n\n/g, ' ');
+				lyrics = lyrics.replace(/\n\n\n/g, ' ');
+				lyrics = lyrics.replace(/ \n\n/g, ' ');
+				lyrics = lyrics.replace(/\n\n/g, ' ');
+				lyrics = lyrics.replace(/ \n/g, ' ');
+				lyrics = lyrics.replace(/\n/g, ' ').split(' ');
+				lyrics.splice(lyrics.length-11, 11);
+				
+				for (var i = 1; i < lyrics.length; i += 2) {
+					lyrics.splice(i, 0, ' ');
+				};
+
+				nowPlaying.lyrics = lyrics;				
+				nowPlaying.tracking = tracking;
+				$location.path('/play');
+			}); //end then()
+	}
+})
 
 myApp.factory('getLyrics', function($http, $q) {
 	return function(trackId) {
--- public/search/search.js
+++ public/search/search.js
@@ -1,68 +1,8 @@
-myApp.controller('SearchCtrl', function ($http, $location, searchSong, getLyrics, nowPlaying) {
+myApp.controller('SearchCtrl', function (querySong, getSong) {
+	$('.query').focus();
 	var searchctrl = this;
-
 	searchctrl.query = '';
 	searchctrl.searchResults;
-
-	searchctrl.searchSong = function(q) {
-		var value = q.trim();
-		if(value.length) {
-			searchSong(value)
-				.then(function(response) {
-					// console.log('ssResponse', response);
-					var tracks = response.message.body.track_list;
-					var goodTracks = [];
-
-					for (var i = 0; i < tracks.length; i++) {
-						if(tracks[i].track.track_spotify_id && tracks[i].track.has_lyrics) {
-							goodTracks.push(tracks[i]);
-						}
-					}
-
-					searchctrl.searchResults = goodTracks;;
-					// searchctrl.searchResults = tracks;;
-				});
-		}	
-	} // end searchSong()
-
-	searchctrl.getSong = function(track) {
-		nowPlaying.artist = track.artist_name;
-		nowPlaying.song = track.track_name;
-		nowPlaying.album = track.album_name;
-		nowPlaying.art = track.album_coverart_500x500;
-		nowPlaying.spotifyId = track.track_spotify_id;
-
-		getLyrics(track.track_id)
-			.then(function(response) {
-
-				// oddly, this had to change from response.data.message..... to response.message....
-				// var tracking = response.data.message.body.lyrics.pixel_tracking_url;
-				// var lyrics = response.data.message.body.lyrics.lyrics_body;
-				var tracking = response.message.body.lyrics.pixel_tracking_url;
-				var lyrics = response.message.body.lyrics.lyrics_body;
-
-				console.log('lyrics_body', lyrics);
-
-				lyrics = lyrics.replace(/’/g, "'");
-				lyrics = lyrics.replace(/‘/g, "'");
-				lyrics = lyrics.replace(/'/g, "\'");
-				lyrics = lyrics.replace(/ \n\n\n/g, ' ');
-				lyrics = lyrics.replace(/\n\n\n/g, ' ');
-				lyrics = lyrics.replace(/ \n\n/g, ' ');
-				lyrics = lyrics.replace(/\n\n/g, ' ');
-				lyrics = lyrics.replace(/ \n/g, ' ');
-				lyrics = lyrics.replace(/\n/g, ' ').split(' ');
-				lyrics.splice(lyrics.length-11, 11);
-				
-				for (var i = 1; i < lyrics.length; i += 2) {
-					lyrics.splice(i, 0, ' ');
-				};
-
-				nowPlaying.lyrics = lyrics;				
-				nowPlaying.tracking = tracking;
-				$location.path('/play');
-			}); //end then()
-	} // end getSong()
-
-	$('.query').focus();
+	searchctrl.searchSong = querySong(searchctrl);
+	searchctrl.getSong = getSong;
 }); //end SearchCtrl
